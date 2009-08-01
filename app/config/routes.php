3adcdd1fa0e1cede9320bc706560618a95dbbfa5
klenwell <klenwell@localhost>
2009-07-31 23:54:35 -0700

    T417: updated stylesheet and edited index.php to accommodate shared hosting

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,4 @@
 <?php
-/* SVN FILE: $Id: routes.php 7690 2008-10-02 04:56:53Z nate $ */
 /**
  * Short description for file.
  *
@@ -7,20 +6,17 @@
  * Routes are very important mechanism that allows you to freely connect
  * different urls to chosen controllers and their actions (functions).
  *
+ * Example (maps / to /pages/display with view file home) :
+ * Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
  */
-/**
- * Here, we are connecting '/' (base path) to controller called 'Pages',
- * its action called 'display', and we pass a param to select the view file
- * to use (in this case, /app/views/pages/home.ctp)...
- */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-/**
- * ...and connect the rest of 'Pages' controller's urls.
- */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-/**
- * Then we connect url '/test' to our test controller. This is helpful in
- * developement.
- */
-	#Router::connect('/test', array('controller' => 'tests', 'action' => 'index'));
+
+    Router::connect('/', array('controller' => 'pages', 'action' => 'home'));
+    Router::connect('/cake', array('controller' => 'pages', 'action' => 'display', 'cake'));
+
+    // Pages Controller (a default)
+    # Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+
+    // Test Controller
+    Router::connect('/test', array('controller' => 'tests', 'action' => 'index'));
+
 ?>
