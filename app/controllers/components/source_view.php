4125cf056db21734a3c1523dc4f4a5307aa9bef9
klenwell <klenwell@localhost>
2009-09-11 22:10:34 -0700

    T440: complete build icon block method

--- app/controllers/components/source_view.php
+++ app/controllers/components/source_view.php
@@ -5,7 +5,10 @@
     Author: Tom at klenwell@gmail.com
     Last Update: $Date$
 
-    Creates links to CakePhp files based on controller values
+    Creates links to CakePhp files based on controller values.  Conceptually,
+    this would make more sense as a helper or an elements, but the controller
+    offers more of the necessary information to infer the links and is simply
+    more than convenient than building an element.
 
     USAGE (in controller)
         var $components = array('SourceView');
@@ -15,7 +18,49 @@
     NOTES
         Assumes CakePhp naming conventions are followed
 
-        Sample Introspective Structure: TBA
+        Sample Introspective Structure:
+
+            [ModelList] => Array
+                (
+
+                    [SimpleRecord] => Array
+                        (
+                            [path] => models/simple_record.php
+                            [behaviors] => Array
+                                (
+                                    [Normalizer] => models/behaviors/normalizer.php
+                                )
+
+                        )
+
+                )
+
+            [ControllerList] => Array
+                (
+                    [Demo] => controllers/demo_controller.php
+                )
+
+            [ComponentList] => Array
+                (
+                    [Twitter] => controllers/components/twitter.php
+                    [Sample] => controllers/components/sample.php
+                    [Gatekeeper] => controllers/components/gatekeeper.php
+                    [SourceView] => controllers/components/source_view.php
+                )
+
+            [ViewList] => Array
+                (
+                    [layout] => views/layouts/default.ctp
+                    [views] => views/demo/sandbox.ctp
+                )
+
+            [HelperList] => Array
+                (
+                    [Html] => views/helpers/html.php
+                    [Form] => views/helpers/form.php
+                )
+
+
 */
 
 
@@ -25,11 +70,13 @@ class SourceViewComponent extends Object
     var $base_url = 'http://code.google.com/p/cakewell/source/browse/app/';
     var $base_img_url = '/img/source_icons/';
 
-    var $Ctrl = null;
     var $ModelList = array();
+    var $ControllerList = array();
     var $ComponentList = array();
     var $ViewList = array();
     var $HelperList = array();
+
+    var $Ctrl = null;
 
     // called before Controller:beforeFilter()
     function initialize(&$controller)
@@ -40,6 +87,18 @@ class SourceViewComponent extends Object
     function startup(&$controller)
     {
         $this->Ctrl = $controller;
+    }
+
+    function link_icon($name, $type, $app_path)
+    {
+        $link_t = <<<XHTML
+<a class="%s_source" href="%s%s" onclick="window.open(this.href,'_blank');return false;">
+    <img class="source_view_icon" src="%s%s.png" alt="%s" title="source view for %s %s" />
+</a>
+XHTML;
+
+        return sprintf($link_t, $type, $this->base_url, $app_path,
+                        $this->base_img_url, $type, $type, $name, $type);
     }
 
     function build_icon_block()
@@ -54,7 +113,36 @@ class SourceViewComponent extends Object
 </div>
 XHTML;
 
+        $ModelHtml = array();
+        $ControllerHtml = array();
+        $ViewHtml = array();
+
         $this->introspect();
+
+        foreach ( $this->ModelList as $name => $Data )
+        {
+            $ModelHtml[] = $this->link_icon($name, 'model', $Data['path']);
+
+            foreach ( $Data['behaviors'] as $name => $path )
+                $ModelHtml[] = $this->link_icon($name, 'behavior', $path);
+        }
+
+        foreach ( $this->ControllerList as $name => $path )
+            $ControllerHtml[] = $this->link_icon($name, 'controller', $path);
+
+        foreach ( $this->ComponentList as $name => $path )
+            $ControllerHtml[] = $this->link_icon($name, 'component', $path);
+
+        foreach ( $this->ViewList as $name => $path )
+            $ViewHtml[] = $this->link_icon($name, 'view', $path);
+
+        foreach ( $this->HelperList as $name => $path )
+            $ViewHtml[] = $this->link_icon($name, 'helper', $path);
+
+        return sprintf($html_t,
+                       implode("\n", $ModelHtml),
+                       implode("\n", $ControllerHtml),
+                       implode("\n", $ViewHtml));
     }
 
     function introspect()
@@ -67,31 +155,67 @@ XHTML;
             {
                 if ( !isset($this->Ctrl->$model_name) ) continue;
                 $Model = $this->Ctrl->$model_name;
-                $mpath = sprintf('%s%s.php', MODELS, low(Inflector::underscore($Model->name)));
-                if ( !file_exists($mpath) ) continue;
-                $this->ModelList[$Model->name] = array( 'path' => $mpath, 'behaviors' => array() );
+                $model_app_path = sprintf('%s/%s.php', 'models', Inflector::underscore($Model->name));
+                $model_full_path = sprintf('%s%s', APP, $model_app_path);
+                if ( !file_exists($model_full_path) ) continue;
+                $this->ModelList[$Model->name] = array( 'path' => $model_app_path, 'behaviors' => array() );
 
                 // look for behaviors
                 if ( is_array($Model->Behaviors->_attached) )
                 {
                     foreach ( $Model->Behaviors->_attached as $behavior )
                     {
-                        $bpath = sprintf('%sbehaviors/%s.php', MODELS, strtolower($behavior));
-                        $this->ModelList[$Model->name]['behaviors'][$behavior] = $bpath;
+                        $behavior_app_path = sprintf('%s/%s/%s.php',
+                            'models', 'behaviors', Inflector::underscore($behavior));
+                        $behavior_full_path = sprintf('%s%s', APP, $behavior_app_path);
+                        if ( !file_exists($behavior_full_path) ) continue;
+                        $this->ModelList[$Model->name]['behaviors'][$behavior] = $behavior_app_path;
                     }
                 }
             }
         }
         #debug($this->ModelList);
 
-        # introspect components
-        foreach ( $this->Ctrl->components as $comp_name )
-        {
-        }
-    }
+        # intropect controller
+        $ctrl_fname = Inflector::underscore($this->Ctrl->name);
+        $ctrl_app_path = sprintf('%s/%s_controller.php', 'controllers', $ctrl_fname);
+        $ctrl_full_path = sprintf('%s%s', APP, $ctrl_app_path);
+        if ( file_exists($ctrl_full_path) ) $this->ControllerList[$this->Ctrl->name] = $ctrl_app_path;
 
-    function icon_block($repo_base_url)
-    {
+        # introspect components
+        if ( empty($this->Ctrl->components) ) $this->Ctrl->components = array();
+        foreach ( $this->Ctrl->components as $component )
+        {
+            $comp_app_path = sprintf('%s/%s/%s.php',
+                'controllers', 'components', Inflector::underscore($component));
+            $comp_full_path = sprintf('%s%s', APP, $comp_app_path);
+            if ( !file_exists($comp_full_path) ) continue;
+            $this->ComponentList[$component] = $comp_app_path;
+        }
+
+        # introspect layout and views
+        if ( empty($this->Ctrl->layout) ) $this->Ctrl->layout = 'default';
+        $layout_app_path = sprintf('%s/%s/%s.ctp', 'views', 'layouts', $this->Ctrl->layout);
+        $layout_full_path = sprintf('%s%s', APP, $layout_app_path);
+        if ( file_exists($layout_full_path) ) $this->ViewList['layout'] = $layout_app_path;
+
+        $view_app_path = sprintf('%s/%s/%s.ctp', 'views', $this->Ctrl->viewPath, $this->Ctrl->action);
+        $view_full_path = sprintf('%s%s', APP, $view_app_path);
+        if ( file_exists($view_full_path) ) $this->ViewList['view'] = $view_full_path;
+
+        # introspect helpers
+        if ( empty($this->Ctrl->helpers) ) $this->Ctrl->helpers = array();
+        foreach ( $this->Ctrl->helpers as $helper )
+        {
+            $helper_app_path = sprintf('%s/%s/%s.php',
+                'views', 'helpers', Inflector::underscore($helper));
+            $helper_full_path = sprintf('%s%s', APP, $comp_app_path);
+            if ( !file_exists($helper_full_path) ) continue;
+            $this->HelperList[$helper] = $helper_app_path;
+        }
+
+        #debug($this);
+        return;
     }
 
     function test()
