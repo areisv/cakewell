64edffb3e599dfea778b9633b7896ae1ee971407
klenwell <klenwell@localhost>
2009-10-05 23:08:24 -0700

    T462: added full stack test for login_request in auth component test

--- app/plugins/authwell/tests/cases/components/auth.test.php
+++ app/plugins/authwell/tests/cases/components/auth.test.php
@@ -19,10 +19,20 @@ class AuthwellCtrl {
 
 class AuthComponentTest extends CakeTestCase {
 
+    var $fixtures = array(
+        'plugin.authwell.authwell_user',
+        'plugin.authwell.authwell_role',
+        'plugin.authwell.authwell_privilege',
+        'plugin.authwell.authwell_user_authwell_role',
+        'plugin.authwell.authwell_role_authwell_privilege',
+    );
+
     function setUp()
     {
         $this->AuthComponent = new AuthComponent();
-        $Ctrl = new AuthwellCtrl();
+        $Ctrl = new AuthwellController();
+        # This does not work as it seems the fixture db has not yet been created
+        #$Ctrl->constructClasses();
         $this->AuthComponent->initialize($Ctrl);
     }
 
@@ -33,7 +43,7 @@ class AuthComponentTest extends CakeTestCase {
 
     function testInstance() {
         $this->assertTrue(is_a($this->AuthComponent, 'AuthComponent'));
-        $this->assertTrue(is_a($this->AuthComponent->Ctrl, 'AuthwellCtrl'));
+        $this->assertTrue(is_a($this->AuthComponent->Ctrl, 'AuthwellController'));
     }
 
     function testDiffDotpaths() {
@@ -75,6 +85,36 @@ class AuthComponentTest extends CakeTestCase {
             $this->assertEqual(
                 $this->AuthComponent->_privilege_has_access($T_[0], $T_[1]),
                 $T_[2] );
+    }
+
+    function testLoginRequest() {
+
+        // first save a user
+        $email = 'test@cakewell.com';
+        $pw = 'password';
+        $Model = ClassRegistry::init('Authwell.AuthwellUser');
+        $RecordObj = new AuthwellUserRecord($Model);
+        $Record = $RecordObj->create(array('email'=>$email,
+            'password'=>$Model->password($pw)));
+        $this->assertTrue($Model->save($Record));
+
+        // then call
+        $FormData = array(
+            'AuthwellUser' => array(
+                'email' => $email,
+                'password' => $pw
+            )
+        );
+
+        /* To use the controller Session component, we must construct its
+           helper classes.  However, we can't do it in the setup (see note
+           above, so we do it here */
+        $this->AuthComponent->Ctrl->constructClasses();
+        $is_logged_in = $this->AuthComponent->login_request($FormData);
+        $SessionUser = $this->AuthComponent->Ctrl->Session->read('Authwell.User');
+
+        $this->assertTrue($is_logged_in);
+        $this->assertEqual($SessionUser['email'],$email);
     }
 }
 
