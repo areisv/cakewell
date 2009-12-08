05958d2d1d562153ab1045e0abb9a591c690fd0f
klenwell <klenwell@localhost>
2009-12-07 21:19:40 -0800

    T518: touched up backend conotroller

--- app/controllers/backend_controller.php
+++ app/controllers/backend_controller.php
@@ -14,27 +14,48 @@ class BackendController extends AppController
     var $name = 'Backend';
     var $uses = null;   // or: array('ModelName');
     var $components = array('RequestHandler', 'Gatekeeper');
-
     var $CacheList = array('views', 'models', 'persistent');
 
     function index()
     {
-        // code
-        // To Do ApiHelper
+        App::import('Helper', 'Html'); // loadHelper('Html'); in CakePHP 1.1.x.x
+        $html = new HtmlHelper();
+        $source_link = $html->link( 'check out',
+            'http://code.google.com/p/cakewell/source/checkout',
+            array('target'=>'_blank') );
+        $browse_link = $html->link( 'browse',
+            'http://code.google.com/p/cakewell/source/browse/app/controllers/backend_controller.php',
+            array('target'=>'_blank') );
+        $menu = $html->link('demo controller', '/demo');
 
-        // output
-        $this->set('content_for_layout', 'backend controller: to do ApiHelper');
-        $this->render('/layouts/blank');
+        $content = <<<XHTML
+<p>The backend controller offers basic administrative control over logs and
+cache files.</p>
+<p>It is not currently set up to run in production for security reasons.  To
+use it, %s or %s the source code.</p>
+XHTML;
+
+        $this->set('header', 'Cakewell Backend');
+        $this->set('content', sprintf($content, $source_link, $browse_link));
+        $this->set('menu', $menu);
+        $this->render('/demo/index');
     }
 
     function logs($subaction=null, $p2=null)
     {
+        # restrict in production
+        $this->Gatekeeper->restrict_from_app_modes( array('production'),
+            '/backend/',
+            'this action is blocked in production');
+
         $max_limit = 100;
+        $default_limit = 20;
 
         # sanity check limit
         if ( is_null($p2) || !is_numeric($p2) )
-            $p2=25;
-        if ( $p2 > $max_limit ) $p2 = $max_limit;
+            $p2 = $default_limit;
+        if ( $p2 > $max_limit )
+            $p2 = $max_limit;
 
         # reset logs
         if ( isset($this->params['form']['reset']) )
@@ -54,7 +75,7 @@ class BackendController extends AppController
     <a href="/backend/logs/tail/10">/backend/logs/tail/10</a>
 </h5>
 XHTML;
-            $this->set('header', 'CakePhp Log Test');
+            $this->set('header', 'CakePhp Log Interface');
             $this->set('content', sprintf($content, $this->here));
             $this->set('menu', '<a href="/backend/logs">Backend::Logs</a>');
             $this->render('/demo/index');
@@ -89,11 +110,10 @@ XHTML;
             subdirectories.  Otherwise, just the views (clearCache default
             behavior).
         */
-        // restrict in production until Authwell ready
+        # restrict in production until Authwell ready
         $this->Gatekeeper->restrict_from_app_modes( array('production'),
             '/backend/',
             'this action is blocked until the Auth component is complete');
-
 
         $SubdirectoryList = $this->CacheList;
         $ClearedDirs = array();
@@ -174,7 +194,6 @@ XHTML;
 
         return 1;
     }
-
 }
 
 ?>
