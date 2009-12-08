eb13a3db429b229b40a12f254891cd02b7a311d3
klenwell <klenwell@localhost>
2009-12-07 20:58:52 -0800

    T518: added basic logging interface to backend controller

--- app/controllers/backend_controller.php
+++ app/controllers/backend_controller.php
@@ -27,6 +27,59 @@ class BackendController extends AppController
         $this->render('/layouts/blank');
     }
 
+    function logs($subaction=null, $p2=null)
+    {
+        $max_limit = 100;
+
+        # sanity check limit
+        if ( is_null($p2) || !is_numeric($p2) )
+            $p2=25;
+        if ( $p2 > $max_limit ) $p2 = $max_limit;
+
+        # reset logs
+        if ( isset($this->params['form']['reset']) )
+        {
+            $this->_reset_log('error');
+            $this->_reset_log('debug');
+            $this->set('alert', 'logs have been reset');
+        }
+
+        # subactions
+        if ( $subaction == 'test' )
+        {
+            $this->log("test debug log ({$this->here})", LOG_DEBUG);
+            $content = <<<XHTML
+<h4>The message below has been added to the debug log: <tt>Log Test: %s</tt></h4>
+<h5>To view the error and debug logs, see
+    <a href="/backend/logs/tail/10">/backend/logs/tail/10</a>
+</h5>
+XHTML;
+            $this->set('header', 'CakePhp Log Test');
+            $this->set('content', sprintf($content, $this->here));
+            $this->set('menu', '<a href="/backend/logs">Backend::Logs</a>');
+            $this->render('/demo/index');
+        }
+        else
+        {
+            # log (too noisy)
+            #$this->log("Tail last $p2 log entries");
+
+            # open error and debug log file
+            $ErrorLines = $this->_read_log('error');
+            $DebugLines = $this->_read_log('debug');
+
+            # collect last $p2 lines
+            $ErrorLines = array_slice($ErrorLines, 0, $p2);
+            $DebugLines = array_slice($DebugLines, 0, $p2);
+            #debug($ErrorLines);
+
+            # prepare view
+            $this->set('limit', $p2);
+            $this->set('ErrorLines', $ErrorLines);
+            $this->set('DebugLines', $DebugLines);
+            $this->render('log');
+        }
+    }
 
     function clear_cache($all=0)
     {
@@ -76,6 +129,50 @@ class BackendController extends AppController
         $this->set('data', $ClearedDirs);
         $this->set('menu', '');
         $this->render('/demo/report');
+    }
+
+    function _read_log($log_name)
+    {
+        $LogLines = array();
+        $fpath_t = '%s/%s.log';
+        $fpath = sprintf($fpath_t, LOGS, $log_name);
+
+        if ( !$Fp = fopen($fpath, "r") )
+        {
+            trigger_error("unable to open log '$fpath'", E_USER_WARNING);
+            return array();
+        }
+
+        while ( ! feof($Fp) )
+        {
+            $line = fgets($Fp);
+            if ( strlen(trim($line)) )
+                $LogLines[] = $line;
+        }
+
+        fclose($Fp);
+        $LogLines = array_reverse($LogLines);
+        return $LogLines;
+    }
+
+    function _reset_log($log_name)
+    {
+        $fpath_t = '%s/%s.log';
+        $fpath = sprintf($fpath_t, LOGS, $log_name);
+
+        if ( !$Fp = fopen($fpath, "w") )
+        {
+            trigger_error("unable to open log '$fpath'", E_USER_WARNING);
+            return 0;
+        }
+
+        fwrite($Fp, '');
+        fclose($Fp);
+
+        $message = sprintf('Resetting %s log', $log_name);
+        $this->log($message, $log_name);
+
+        return 1;
     }
 
 }
