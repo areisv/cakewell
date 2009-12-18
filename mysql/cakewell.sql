994ca731c1b4677653d952e6dd0e3910bb09fa35
klenwell <klenwell@localhost>
2009-12-17 22:00:04 -0800

    T521: added migration for new simple log tables

--- mysql/cakewell.sql
+++ mysql/cakewell.sql
@@ -46,3 +46,45 @@ CREATE TABLE IF NOT EXISTS `comments` (
   KEY `parent_id` (`parent_id`),
   KEY `created` (`created`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
+
+-- --------------------------------------------------------
+--
+-- MIGRATIONS (a very crude implementation)
+-- post-install updates
+--
+
+-- SPRINT v1s7
+-- adding simple_logs and simple_log_types tables
+-- development: 2009.12.17
+-- production: TBA
+-- DROP TABLE IF EXISTS `simple_logs`;
+CREATE TABLE IF NOT EXISTS `simple_logs` (
+    id int(11) NOT NULL auto_increment,
+    type_id int(11) default NULL,
+    keyword varchar(16) NOT NULL,
+    message varchar(255) NOT NULL,
+    created DATETIME,
+    PRIMARY KEY  (`id`),
+    KEY `k_type_id` (`type_id`),
+    KEY `k_keyword` (`keyword`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+INSERT INTO `simple_logs` (`id`, `type_id`, `keyword`, `message`, `created`) VALUES
+(1, 1, 'database', 'creating simple_records and simple_log_types databases', NOW());
+
+-- DROP TABLE IF EXISTS `simple_log_types`;
+CREATE TABLE IF NOT EXISTS `simple_log_types` (
+    id int(11) NOT NULL auto_increment,
+    type varchar(16) NOT NULL,
+    updated DATETIME,
+    created DATETIME,
+    PRIMARY KEY  (`id`),
+    KEY `k_type` (`type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+INSERT INTO `simple_log_types` (`id`, `type`, `created`) VALUES
+(1, 'system', NOW()),
+(2, 'error', NOW()),
+(3, 'warning', NOW()),
+(4, 'info', NOW()),
+(5, 'debug', NOW());
